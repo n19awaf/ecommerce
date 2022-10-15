c3376e63d6bc19c5225a137faeb1c73dc1fb19b0
nawaf <nawaf13-@hotmail.com>
2022-10-15 22:10:29 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin= require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+
+
+module.exports = {
+
+    mode: 'development',
+
+    entry: {
+
+      main: path.resolve(__dirname, './src/index.js'),
+
+    },
+
+    output: {
+
+        filename: 'main.js',
+
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+
+    },
+    
+
+    devServer: {
+
+      static: {
+
+        directory: path.join(__dirname, 'build'),
+
+    },
+
+      port: 1233,
+  
+      devMiddleware: {
+        writeToDisk: true
+     }
+  
+    },
+  
+
+    module: {
+
+      rules: [
+ 
+        {
+ 
+          test: /\.html$/,
+ 
+          use: [
+ 
+            { 
+ 
+   loader: 'html-loader',
+ 
+              options: {
+ 
+             minimize: true,
+ 
+           }
+          }
+ 
+          ],
+ 
+        },
+ 
+        {
+ 
+          test: /\.css$/,
+ 
+          use: [
+ 
+              //'style-loader',
+ 
+          MiniCssExtractPlugin.loader,
+            'css-loader'
+ 
+          ]
+ 
+        },
+        {
+
+          test: /\.(png|svg|jpe?g|gif)$/,
+  
+          use: [
+  
+            {
+  
+              loader: "file-loader", 
+  
+              options: {
+  
+              name: '[name].[ext]',
+  
+              outputPath: "images",
+  
+              }
+            }
+          ]
+        }  
+        
+ 
+      ]
+ 
+    },
+ 
+   plugins: [
+ 
+     new HtmlWebpackPlugin({
+ 
+       filename: "index.html",
+ 
+       template: "./src/index.html",
+ 
+     }),
+ 
+     new MiniCssExtractPlugin({filename: "css/style.css"}),
+     new OptimizeCSSAssetsPlugin({}),
+ 
+    ],
+   };
