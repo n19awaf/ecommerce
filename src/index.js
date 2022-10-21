7ff0882fd33abd15e30d0c737c889f099ee9ac26
nawaf <nawaf13-@hotmail.com>
2022-10-21 18:38:48 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import './css/style.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/all.min'
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
